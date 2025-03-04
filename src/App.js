c21d223891c1c90dbc5169a29a18a4ddf4240f94
Giridhar <kornigiridhar123@gmail.com>
2025-03-04 16:58:58 +0530

    added some code to sign and login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import VendorSignup from './Components/admin/VendorSignup';
-import VendorLogin from './Components/admin/VendorLogin';
-import UserSignup from './Components/User/UserSignup';
-import UserLogin from './Components/User/UserLogin';
+import VendorSignup from './pages/admin/VendorSignup';
+import VendorLogin from './pages/admin/VendorLogin';
+import UserSignup from './pages/User/UserSignup';
+import UserLogin from './pages/User/UserLogin';
+import Navbar from './Components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/VendorSignup" element={<VendorSignup />} />
         <Route path="/VendorLogin" element={<VendorLogin />} />
---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css"; // Import the CSS file
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="nav-link">Home</Link>
+      <Link to="/about" className="nav-link">About</Link>
+      <Link to="/contact" className="nav-link">Contact</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
